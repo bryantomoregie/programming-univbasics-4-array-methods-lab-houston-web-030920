589f1d7f722ec63cb1ff301635fe8544a655eb6d
Bryant Omoregie <unclebryant@gmail.com>
2020-02-10 20:58:42 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,7 +1,5 @@
 def using_include(array, element)
-  array = ["wow", "I", "am", "really", "learning", "arrays!"]
-element1 = array.include?( "wow" )
-element2 = array.include?( "Tardis" )
+  array.include?(element)
 end
 
 def using_sort(array)
